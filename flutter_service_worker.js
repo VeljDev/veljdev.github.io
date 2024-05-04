702cdc104cad26028ee00b9f70f2b3e0a879a2f2
Velj <veljko.markovic@student.supsi.ch>
2024-05-04 13:29:55 +0200

    New stable release

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -18,8 +18,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "9c8090e50eff5a6bcb8dbcd50a57dba1
 "assets/assets/resources-page.png": "e3a22ce8beafce652eb979f0b1af08c7",
 "assets/assets/united-states.png": "c7ef7aeea67ae3612237fefcaa3b8db3",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "f849253831d01db2416a9e965a82f90b",
-"assets/NOTICES": "ca72059a6a59555d6f91a530aaa3e9f8",
+"assets/fonts/MaterialIcons-Regular.otf": "82ef970ae6fc08501be92f4b5aba6746",
+"assets/NOTICES": "0b98c8c34fd71f432bc82d85a1894776",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
 "assets/packages/flutter_map/lib/assets/flutter_map_logo.png": "208d63cc917af9713fc9572bd5c09362",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
@@ -39,9 +39,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "9c8090e50eff5a6bcb8dbcd50a57dba1
 "icons/Icon-512.png": "b5f5069770b092c53952b07d9bbfa5f1",
 "icons/Icon-maskable-192.png": "fa398695824425067650c074b78e06e9",
 "icons/Icon-maskable-512.png": "b5f5069770b092c53952b07d9bbfa5f1",
-"index.html": "512ddb61914638b687a68319a833a12b",
-"/": "512ddb61914638b687a68319a833a12b",
-"main.dart.js": "a820513e1b191a1d570eb9bb5b3fc1cb",
+"index.html": "3708789080a71678bff89fbda8a5f285",
+"/": "3708789080a71678bff89fbda8a5f285",
+"main.dart.js": "70be3d2a92f107a4ce8f471803742ff2",
 "manifest.json": "71dd3d0f40536304a358154e57cb4515",
 "version.json": "cdc737597fec71940e3f46dfcc38d3fd"};
 // The application shell files that are downloaded before a service worker can
